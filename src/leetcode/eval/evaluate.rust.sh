8c40daeb2e4374c0d9eb668ff76589aeb4187219
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2024-05-27 18:55:04 +0100

    feat(neetcode/rust): add test script

---
+++ src/leetcode/eval/evaluate.rust.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Check if the data_path and dir_path argument is provided
+if [ $# -ne 2 ]; then
+	echo "Usage: $0 <data_path> <dir_path>"
+	exit 1
+fi
+
+data_path="$1"
+dir_path="$2"
+
+# Check if the datasets exist
+if [ ! -d "$data_path" ]; then
+	echo "data path not found: $data_path"
+	exit 1
+fi
+
+# Check if the implementations exist
+if [ ! -d "$dir_path" ]; then
+	echo "dir path not found: $dir_path"
+	exit 1
+fi
+
+data_path=$(realpath "$data_path")
+dir_path=$(realpath "$dir_path")
+
+relative_data_path=$(realpath --relative-to="$dir_path" "$data_path")
+
+(
+	cd "$dir_path" || exit 1
+	cargo run -- "$relative_data_path"
+)
