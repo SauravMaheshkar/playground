8beb5b11bb8721c4269909b546788a93e196b681
Saurav Maheshkar <sauravvmaheshkar@gmail.com>
2025-05-28 23:11:55 +0100

    does whatever a spider can

--- src/shell/main.c
+++ src/shell/main.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <readline/readline.h>
 #include <readline/history.h>
+#include <sys/wait.h>
+#include <unistd.h>
 
 #define PROMPT "[spidey-sh]λ "
 
@@ -26,6 +28,28 @@ int main() {
     }
     if (strcmp(cmd, "exit") == 0) {
       QUIT = true;
+    } else if (strlen(cmd) > 0) {
+      char *argv[64];
+      int argc = 0;
+      char *token = strtok(cmd, " \t\n");
+      while (token && argc < 63) {
+        argv[argc++] = token;
+        token = strtok(NULL, " \t\n");
+      }
+      argv[argc] = NULL;
+      if (argc > 0) {
+        pid_t pid = fork();
+        if (pid == 0) { // Kind
+          execvp(argv[0], argv);
+          perror("ERROR");
+          exit(EXIT_FAILURE);
+        } else if (pid > 0) { // Eltern
+          int status;
+          waitpid(pid, &status, 0);
+        } else {
+          perror("fork");
+        }
+      }
     }
     free(cmd);
     line++;
